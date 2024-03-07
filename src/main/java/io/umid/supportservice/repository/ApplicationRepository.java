3565caa58a7f29bfe2fb737ec9b41495547f35fc
umid <umid1504@gmail.com>
2024-03-07 12:57:30 +0500

    feat: Application,User,Role repositories

---
+++ src/main/java/io/umid/supportservice/repository/ApplicationRepository.java
@@ -0,0 +1,9 @@
+package io.umid.supportservice.repository;
+
+import io.umid.supportservice.model.Application;
+import org.springframework.data.repository.Repository;
+
+public interface ApplicationRepository extends Repository<Application, Integer> {
+
+
+}
---
+++ src/main/java/io/umid/supportservice/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package io.umid.supportservice.repository;
+
+import io.umid.supportservice.model.Role;
+import org.springframework.data.repository.Repository;
+
+public interface RoleRepository extends Repository<Role, Integer> {
+
+
+}
