289e66e6be63465ccb84b328775b54eb0136a6df
umid <umid1504@gmail.com>
2024-03-09 12:52:38 +0500

    feat: RefreshTokenJweDeserializer

---
+++ src/main/java/io/umid/supportservice/security/RefreshTokenJweDeserializer.java
@@ -0,0 +1,56 @@
+package io.umid.supportservice.security;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWEDecrypter;
+import com.nimbusds.jwt.EncryptedJWT;
+import com.nimbusds.jwt.JWTClaimsSet;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.UUID;
+import java.util.function.Function;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class RefreshTokenJweDeserializer implements Function<String, RefreshToken> {
+
+    private final JWEDecrypter jweDecrypter;
+
+    @Override
+    public RefreshToken apply(String s) {
+
+        try {
+            EncryptedJWT encryptedJWT = EncryptedJWT.parse(s);
+            encryptedJWT.decrypt(jweDecrypter);
+            JWTClaimsSet jwtClaimsSet = encryptedJWT.getJWTClaimsSet();
+
+            if (hasNotExpired(jwtClaimsSet)) {
+                return new RefreshToken(
+                        UUID.fromString(jwtClaimsSet.getJWTID()),
+                        jwtClaimsSet.getSubject(),
+                        jwtClaimsSet.getIssueTime(),
+                        jwtClaimsSet.getExpirationTime()
+                );
+            }
+
+            return null;
+        } catch (ParseException e) {
+            log.warn("Exception during verifying refresh token. Invalid token: {}", s);
+            throw new RuntimeException(e);
+        } catch (JOSEException e) {
+            log.warn("Exception during decrypting refresh token. Invalid token: {}", s);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean hasNotExpired(JWTClaimsSet jwtClaimsSet) {
+        Date expiry = jwtClaimsSet.getExpirationTime();
+        log.debug("Expiration time of jwe access token: {}", expiry);
+        return expiry.before(Date.from(Instant.now()));
+    }
+}
