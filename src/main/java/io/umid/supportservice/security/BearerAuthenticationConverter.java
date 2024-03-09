e64c37b5040a1147dac3cc60f35b5b269d7270ef
umid <umid1504@gmail.com>
2024-03-09 19:29:49 +0500

    feat: use pre-authenticated token because no password check is needed

--- src/main/java/io/umid/supportservice/security/BearerAuthenticationConverter.java
+++ src/main/java/io/umid/supportservice/security/BearerAuthenticationConverter.java
@@ -4,9 +4,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationConverter;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
@@ -28,12 +28,11 @@ public class BearerAuthenticationConverter implements AuthenticationConverter {
             AccessToken accessToken = accessTokenDeserializer.apply(tokenRaw);
 
             if (accessToken != null) {
-                log.debug("Deserializer access token of subject: {}", accessToken.subject());
+                log.debug("Deserialized access token of subject: {}", accessToken.subject());
 
                 request.setAttribute("authByJwt", "true");
 
-                return UsernamePasswordAuthenticationToken
-                        .unauthenticated(accessToken.subject(), null);
+                return new PreAuthenticatedAuthenticationToken(accessToken, true);
             }
 
             log.debug("Couldn't deserialize bearer token");
