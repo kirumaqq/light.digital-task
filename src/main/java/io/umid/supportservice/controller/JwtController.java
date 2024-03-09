45e38f77b9f231cb13a630a7a65140f5ee9f5057
umid <umid1504@gmail.com>
2024-03-09 12:14:50 +0500

    feat: add JwtController and JwtService

---
+++ src/main/java/io/umid/supportservice/controller/JwtController.java
@@ -0,0 +1,43 @@
+package io.umid.supportservice.controller;
+
+import io.umid.supportservice.dto.JwtResponse;
+import io.umid.supportservice.service.JwtService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/jwt")
+public class JwtController {
+
+    private final JwtService jwtService;
+
+    @GetMapping("/token")
+    public JwtResponse getTokens(@AuthenticationPrincipal UserDetails userDetails) {
+        return jwtService.receiveTokens(userDetails);
+    }
+
+    @GetMapping
+    public JwtResponse refresh(@RequestHeader(name = HttpHeaders.AUTHORIZATION) String auth) {
+
+
+        if (auth.startsWith("Bearer ")) {
+            String token = auth.substring(7);
+            JwtResponse jwtResponse = jwtService.refreshToken(token);
+
+            if (jwtResponse != null) {
+                return jwtResponse;
+            }
+        }
+
+        throw new AccessDeniedException("Refresh token is invalid");
+    }
+}
