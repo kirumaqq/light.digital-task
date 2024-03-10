e592fd0e4ef45952260fb3ff31b16f7cf0601635
umid <umid1504@gmail.com>
2024-03-10 19:42:23 +0500

    fix: unique_name constraint already exists

--- src/main/resources/db/changelog/changelog-constraints/2-unique-name-applications.sql
+++ src/main/resources/db/changelog/changelog-constraints/2-unique-name-applications.sql
@@ -1,4 +1,4 @@
 --liquibase formatted sql
 
 --changeset umid:2
-ALTER TABLE applications ADD CONSTRAINT unique_name UNIQUE (name);
+ALTER TABLE applications ADD CONSTRAINT unique_app UNIQUE (name);
