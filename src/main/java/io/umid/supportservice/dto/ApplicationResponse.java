ea5eef5536ea6ce54eac87ea9a5bfb25c2922f00
umid <umid1504@gmail.com>
2024-03-11 13:08:12 +0500

    feat: include id in response dto

--- src/main/java/io/umid/supportservice/dto/ApplicationResponse.java
+++ src/main/java/io/umid/supportservice/dto/ApplicationResponse.java
@@ -5,7 +5,7 @@ import io.umid.supportservice.model.ApplicationStatus;
 import java.sql.Date;
 
 public record ApplicationResponse(
-
+        Integer id,
         String name,
         String phoneNumber,
         ApplicationStatus status,
